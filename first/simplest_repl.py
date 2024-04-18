8d0f0f0123c48cedf6f504144afab643db469f96
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-04-18 09:05:31 +0700

    add materials for lesson 6

---
+++ first/simplest_repl.py
@@ -0,0 +1,13 @@
+﻿'''
+	Простейший REPL
+'''
+
+command : str = ''
+while True:
+	command = input('> ')
+	if command == 'exit':
+		break
+	elif command == 'hello':
+		print('Привет мир!')
+	else:
+		print(f'Неопознанная команда: {command}')
