4343996d8f0494c26732fd905002849037eeffb9
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-04-27 16:58:45 +0700

    fix replaced lesson 12 task 1 solution

--- py1_12/12_1.py
+++ py1_12/12_1.py
@@ -3,114 +3,202 @@
     Добавлены логирование и assertы
 '''
 
-from random import randint
+import random
+import logging
+
+
+logging.basicConfig(level = logging.DEBUG, filename = "main.log", filemode = "w",
+                    format = "%(asctime)s %(levelname)s %(message)s")
 
 class Unit:
     def __init__(self, name : str, attack : int, defence : int, health : int,
         speed : int, min_damage : int, max_damage : int):
-        self.name : str = name # Название боевой единицы
-        self.attack : int = attack # Атака
-        self.defence : int = defence # Защита
-        self.health : int = health # Максимальное здоровье
-        self.speed : int = speed # Скорость передвижения
-        self.min_damage : int = min_damage # Минимальный урон
-        self.max_damage : int = max_damage # Максимальный урон
+        logging.debug(f'Вызван конструктор Unit с параметрами {name}, {attack}, {defence}, '
+                          f'{health}, {speed}, {min_damage}, {max_damage}')
+        self.__name : str = name # Название боевой единицы
+        self.__attack : int = attack # Атака
+        self.__defence : int = defence # Защита
+        self.__health : int = health # Максимальное здоровье
+        self.__speed : int = speed # Скорость передвижения
+        self.__min_damage : int = min_damage # Минимальный урон
+        self.__max_damage : int = max_damage # Максимальный урон
+        logging.info(f'Создан объект {Unit}')
+        assert self.__attack >= 0 and self.__defence >= 0 and self.__health >= 1 \
+            and self.__speed >= 0 and self.__min_damage >= 0 and self.__max_damage >= min_damage
+        
     def print(self) -> None:
         ''' Вывод информации о юните '''
-        print(f'Название: {self.name}')
-        print(f'Атака: {self.attack}')
-        print(f'Защита: {self.defence}')
-        print(f'Здоровье: {self.health}')
-        print(f'Скорость: {self.speed}')
-        print(f'Минимальный урон: {self.min_damage}')
-        print(f'Максимальный урон: {self.max_damage}')
+        print(f'Название: {self.__name}')
+        print(f'Атака: {self.__attack}')
+        print(f'Защита: {self.__defence}')
+        print(f'Здоровье: {self.__health}')
+        print(f'Скорость: {self.__speed}')
+        print(f'Минимальный урон: {self.__min_damage}')
+        print(f'Максимальный урон: {self.__max_damage}')
 
 class Castle:
     def __init__(self, name : str, money : int, income : int, unit_income : int,
         available_units : int, units_in_harrizon : int):
-        self.name : str = name # Имя замка
-        self.money : int = money # Количество денег в казне замка
-        self.income : int = income # Прирост денег за день
-        self.unit_income : int = unit_income # Прирост существ в замке за неделю
-        self.available_units : int = available_units # Количество существ, доступных для найма
-        self.units_in_harrizon : int = units_in_harrizon # Количество существ в гарнизоне замка
+        logging.debug(f'Вызван конструктор Castle с параметрами {name}, {money}, {income}, '
+                      f'{unit_income}, {available_units}, {units_in_harrizon}')
+        self.__name : str = name # Имя замка
+        self.__money : int = money # Количество денег в казне замка
+        self.__income : int = income # Прирост денег за день
+        self.__unit_income : int = unit_income # Прирост существ в замке за неделю
+        self.__available_units : int = available_units # Количество существ, доступных для найма
+        self.__units_in_harrizon : int = units_in_harrizon # Количество существ в гарнизоне замка
+        logging.info(f'Создан объект {self}')
+        assert self.__money >= 0 and self.__income >= 0 and self.__unit_income >= 0 \
+            and self.__available_units >= 0 and self.__units_in_harrizon >= 0
+    
+    def get_name(self) -> str:
+        return self.__name
+        
+    def get_units_in_harrizon(self) -> int:
+        return self.__units_in_harrizon
+    
+    def set_units_in_harrizon(self, value) -> None:
+        self.__units_in_harrizon = 0 if value <= 0 else value
+        logging.info(f'У объекта Castle с именем {self.__name} изменено поле units_in_harrizon')
+        assert self.__units_in_harrizon >= 0
+    
     def upgrade_income(self) -> None:
         ''' Увеличивает ежедневный доход замка, затрачивая деньги '''
-        self.money -= 5000
-        self.income += 2000
+        if self.__money >= 5000:
+            self.__money -= 5000
+            self.__income += 2000
+            logging.info(f'Повышен доход в замке {self.__name}')
+        assert self.__money >= 0
+        
     def upgrade_unit_income(self) -> None:
         ''' За плату увеличивает еженедельный прирост существ '''
-        self.money -= 1500
-        self.unit_income += 25
+        if self.__money >= 1500:
+            self.__money -= 1500
+            self.__unit_income += 25
+            logging.info(f'Повышен приход войск в замке {self.__name}')
+        assert self.__money >= 0
+        
     def buy_units(self) -> None:
         ''' Купить часть доступных для найма существ и поместить их в гарнизон '''
-        self.money -= 3000
-        self.available_units -= 20
-        self.units_in_harrizon += 20
+        if self.__money >= 3000 and self.__available_units >= 20:
+            self.__money -= 3000
+            self.__available_units -= 20
+            self.__units_in_harrizon += 20
+            logging.info(f'В замке {self.__name} куплены войска и размещены в гарнизоне')
+        assert self.__money >= 0  and self.__available_units >= 0
+        
     def refill(self) -> None:
         ''' Каждую неделю число доступных для найма существ и денег увеличивается
             на соответствующий им прирост '''
-        self.available_units += self.unit_income
-        self.money += self.income
+        self.__available_units += self.__unit_income
+        self.__money += self.__income
+        logging.info(f'В замке {self.__name} приросло количество денег и существ')
+        
     def print(self) -> None:
         ''' Вывод информации о замке '''
-        print(f'Имя: {self.name}')
-        print(f'Денег в казне: {self.money}')
-        print(f'Доход: {self.income}')
-        print(f'Прирост существ: {self.unit_income}')
-        print(f'Доступно существ для найма: {self.available_units}')
-        print(f'Существ в гарнизоне: {self.units_in_harrizon}')
+        print(f'Имя: {self.__name}')
+        print(f'Денег в казне: {self.__money}')
+        print(f'Доход: {self.__income}')
+        print(f'Прирост существ: {self.__unit_income}')
+        print(f'Доступно существ для найма: {self.__available_units}')
+        print(f'Существ в гарнизоне: {self.__units_in_harrizon}')
+        
 class Hero:
     def __init__(self, name : str, level : int, move_points : int, units_in_army : int,
-        attack : int, defence : int, spell_power : int, knowledge : int):
-        self.name : str = name # Имя героя
-        self.level : int = level # Уровень героя
-        self.move_points: int = move_points # Запас хода
-        self.units_in_army : int = units_in_army # Количество существ в армии героя
-        self.attack : int = attack # Атака
-        self.defence : int = defence # Защита
-        self.spell_power : int = spell_power # Сила заклинаний
-        self.knowledge : int = knowledge # Знания
-    def move(self) -> None:
+        is_in_castle : bool, attack : int, defence : int, spell_power : int, knowledge : int):
+        logging.debug(f'Вызван конструктор Hero с параметрами {name}, {level}, {move_points}, '
+                      f'{units_in_army}, {attack}, {defence}, {spell_power}, {knowledge}')
+        self.__name : str = name # Имя героя
+        self.__level : int = level # Уровень героя
+        self.__move_points: int = move_points # Запас хода
+        self.__units_in_army : int = units_in_army # Количество существ в армии героя
+        self.__is_in_castle : bool = is_in_castle # Находится ли герой в данный момент в замке
+        self.__attack : int = attack # Атака
+        self.__defence : int = defence # Защита
+        self.__spell_power : int = spell_power # Сила заклинаний
+        self.__knowledge : int = knowledge # Знания
+        logging.info(f'Создан объект {self}')
+        assert self.__level >= 0 and self.__move_points >= 0 and self.__units_in_army >= 0 \
+            and self.__attack >= 0 and self.__defence >= 0 and self.__spell_power >= 0 and self.__knowledge >= 0
+        
+    def move(self) -> bool:
         ''' Герой двигается по карте '''
-        self.move_points -= 500
+        if self.__move_points >= 500:
+            self.__move_points -= 500
+            self.__is_in_castle = False
+            logging.info(f'Герой {self.__name} совершил передвижение по карте')
+            assert self.__move_points >= 0
+            return True
+        return False
+    
+    def return_to_castle(self) -> bool:
+        ''' Герой возвращается в ближайший замок '''
+        if self.__move_points >= 500:
+            self.__move_points -= 500
+            self.__is_in_castle = True
+            logging.info(f'Герой {self.__name} вернулся в замок')
+            assert self.__move_points >= 0
+            return True
+        return False
+        
     def rest(self) -> None:
         ''' Герой отдыхает ночью, восстанавливая запас хода '''
-        self.move_points += 2000
-    def level_up(self) -> None:
-        ''' Герой ищет на карте сражения, получая новый уровень и увеличивая атрибут'''
-        self.move_points -= 1500
-        self.units_in_army -= 15
-        self.level += 1
-        match randint(1, 4):
-            case 1:
-                self.attack += 1
-            case 2:
-                self.defence += 1
-            case 3:
-                self.spell_power += 1
-            case 4:
-                self.knowledge += 1
-    def refill_army(self, castle : Castle) -> None:
+        logging.info(f'Герой {self.__name} отдохнул ночью')
+        self.__move_points += 2000
+        
+    def level_up(self) -> bool:
+        ''' Герой ищет на карте сражения, получая новый уровень и увеличивая атрибут '''
+        if self.__move_points >= 1500 and self.__units_in_army >= 15:
+            self.__move_points -= 1500
+            self.__is_in_castle = False
+            self.__units_in_army -= 15
+            self.__level += 1
+            match random.randint(1, 4):
+                case 1:
+                    self.__attack += 1
+                case 2:
+                    self.__defence += 1
+                case 3:
+                    self.__spell_power += 1
+                case 4:
+                    self.__knowledge += 1
+            assert self.__move_points >= 0 and self.__units_in_army >= 0
+            logging.info(f'Герой {self.__name} поднял уровень')
+            return True
+        return False
+                    
+    def take_army_from_castle_harrizon(self, army : int, castle :  Castle) -> bool:
+        ''' Герой забирает армию из гарнизона любого из городов, при условии его
+            нахождения в каком-нибудь из замков и наличии достаточного количества существ '''
+        if self.__is_in_castle and army >= 0 and castle.get_units_in_harrizon() >= army:
+            self.__units_in_army += army
+            castle.set_units_in_harrizon(castle.get_units_in_harrizon() - army)
+            logging.info(f'Герой {self.__name} забрал армию из замка {castle.get_name()}')
+            return True
+        return False
+        
+    def refill_army(self, castle : Castle) -> bool:
         ''' Герой возвращается в замок и пополняет армию '''
-        self.move()
-        castle.units_in_harrizon -= 50
-        self.units_in_army += 50
+        if self.return_to_castle() and self.take_army_from_castle_harrizon(50, castle):
+            assert self.__move_points >= 0
+            return True
+        return False
+        
     def print(self) -> None:
         ''' Вывод информации о герое '''
-        print(f'Имя: {self.name}')
-        print(f'Уровень: {self.level}')
-        print(f'Запас хода: {self.move_points}')
-        print(f'Существ в армии: {self.units_in_army}')
-        print(f'Атака: {self.attack}')
-        print(f'Защита: {self.defence}')
-        print(f'Сила заклинаний: {self.spell_power}')
-        print(f'Знания: {self.knowledge}')
+        print(f'Имя: {self.__name}')
+        print(f'Уровень: {self.__level}')
+        print(f'Запас хода: {self.__move_points}')
+        print(f'Существ в армии: {self.__units_in_army}')
+        print(f'Атака: {self.__attack}')
+        print(f'Защита: {self.__defence}')
+        print(f'Сила заклинаний: {self.__spell_power}')
+        print(f'Знания: {self.__knowledge}')
 
 castle1 = Castle('Бреттония', 15000, 2500, 20, 60, 10)
 castle2 = Castle('Александретта', 10000, 2000, 10, 40, 0)
-hero1 = Hero('Адель', 1, 2000, 50, 1, 0, 2, 2)
-hero2 = Hero('Валеска', 1, 1000, 100, 2, 2, 0, 1)
+hero1 = Hero('Адель', 1, 2000, 50, 1, True, 0, 2, 2)
+hero2 = Hero('Валеска', 1, 1000, 100, False, 2, 2, 0, 1)
 
 castle1.upgrade_income()
 castle1.upgrade_unit_income()
