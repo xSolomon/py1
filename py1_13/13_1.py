8ab5eb8815cf6fee5c5f9ae5a659d9f7ab87a42a
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-05-02 02:03:32 +0700

    add solution and reflection for lesson 13

---
+++ py1_13/13_1.py
@@ -0,0 +1,49 @@
+'''
+    Тестирование функции сортировки различными видами тестов
+'''
+
+import unittest
+from random import randint
+
+class BubbleSortTests(unittest.TestCase):
+    def test_regression_1(self) -> None:
+        test_list : list[int] = [i for i in range(10, 0, -1)]
+        bubble_sort(test_list)
+        self.assertEqual(test_list, [i for i in range(1, 11)])
+        
+    def test_regression_2(self) -> None:
+        test_list : list[int] = [i for i in range(1, 10)]
+        bubble_sort(test_list)
+        self.assertEqual(test_list, [i for i in range(1, 10)])
+    
+    def test_empty_list(self) -> None:
+        test_list : list[int] = []
+        bubble_sort(test_list)
+        self.assertEqual(test_list, [])
+        
+    def test_border_values(self) -> None:
+        test_list : list[int] = [9223372036854775807, -9223372036854775808, 9223372036854775807]
+        bubble_sort(test_list)
+        self.assertEqual(test_list, [-9223372036854775808, 9223372036854775807, 9223372036854775807])
+        
+    def test_random(self) -> None:
+        for i in range(100):
+            test_list : list[int] = [randint(-1000, 1000) for i in range(1000)]
+            sorted_list : list[int] = sorted(test_list)
+            bubble_sort(test_list)
+            self.assertEqual(test_list, sorted_list)
+
+def bubble_sort(int_array : list[int]) -> None:
+    ''' Сортировка массива чисел методом пузырька '''
+    swaps_occured : bool = True
+    for i in range(len(int_array)):
+        swaps_occured = False
+        for i in range(len(int_array) - i - 1):
+            if int_array[i] > int_array[i + 1]:
+                int_array[i], int_array[i + 1] = int_array[i + 1], int_array[i]
+                swaps_occured = True
+        if not swaps_occured:
+            break;
+
+
+unittest.main()
