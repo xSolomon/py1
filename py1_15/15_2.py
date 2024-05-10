06eb5e09ca487b0103238c866503ae9dbc465533
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-05-10 14:13:25 +0700

    fix wrong lesson 15 task 2 solution

--- py1_15/15_2.py
+++ py1_15/15_2.py
@@ -5,7 +5,7 @@ def delete_directory(path : str) -> bool:
     if not os.path.isdir(path):
         return False
     for file_or_dir in os.listdir(path):
-        if os.path.isdir(file_or_dir):
+        if os.path.isdir(os.path.join(path, file_or_dir)):
             return False
     shutil.rmtree(path)
     return True
