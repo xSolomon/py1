660b744ef8a476c9ee9143e5e95501ab33212b96
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-05-14 16:00:23 +0700

    fixed bug in lesson 15 task 1 solution and changed lesson 17 task 1 and 2 solutions to use handwritten listdir func

--- py1_17/17_1.py
+++ py1_17/17_1.py
@@ -1,15 +1,16 @@
 ''' Урок 17, задание 1 '''
 import os
 from PIL import Image
+from directory import directory_content
 
 def change_images_extenstion(from_ext : str, to_ext : str) -> None:
     ''' Изменяет формат всех изображений указанного расширения в текущем каталоге '''
-    for file_or_dir in os.listdir():
-        if os.path.isfile(file_or_dir) and file_or_dir.endswith(from_ext):
-            try:
-                image : Image = Image.open(file_or_dir)
-                if to_ext == '.jpg' and image.mode in ('RGBA', 'P'):
-                    image = image.convert('RGB')
-                image.save(os.path.splitext(file_or_dir)[0] + to_ext)
-            except:
-                print(f'Ошибка при попытке изменения расширения изображения {file_or_dir}')
+    for im in directory_content('.', from_ext, False)[0]:
+        try:
+            image : Image = Image.open(im)
+            if to_ext == '.jpg' and image.mode in ('RGBA', 'P'):
+                image = image.convert('RGB')
+            image.save(os.path.splitext(im)[0] + to_ext)
+        except:
+            print(f'Ошибка при попытке изменения расширения изображения {im}')
+                
--- py1_17/17_2.py
+++ py1_17/17_2.py
@@ -1,22 +1,22 @@
 from PIL import Image, ImageDraw
 import os
+from directory import directory_content
 
 def write_text_in_center_and_save_as(from_ext : str, to_ext : str) -> None:
     ''' Изменяет формат всех изображений указанного расширения в текущем каталоге.
         Также в центре каждого изображения рисует квадрат и пишет внутри этого квадрата текст'''
-    for file_or_dir in os.listdir():
-        if os.path.isfile(file_or_dir) and file_or_dir.endswith(from_ext):
-            try:
-                image : Image = Image.open(file_or_dir)
-                if to_ext == '.jpg' and image.mode in ('RGBA', 'P'):
-                    image = image.convert('RGB')
-                canvas : ImageDraw = ImageDraw.Draw(image)
-                size : tuple = image.size
-                # Квадрат размером 50x50
-                canvas.rectangle([size[0] / 2 - 25,size[1] / 2 - 25, \
-                    size[0] / 2 + 25,size[1] / 2 + 25])
-                canvas.multiline_text((size[0] / 2 - 20, size[1] / 2 - 20), 'Hello,\nWorld!')
-                image.save(os.path.splitext(file_or_dir)[0] + to_ext)
-                del canvas
-            except:
-                print(f'Ошибка при попытке изменения расширения изображения {file_or_dir}')
+    for im in directory_content('.', from_ext, False)[0]:
+        try:
+            image : Image = Image.open(im)
+            if to_ext == '.jpg' and image.mode in ('RGBA', 'P'):
+                image = image.convert('RGB')
+            canvas : ImageDraw = ImageDraw.Draw(image)
+            size : tuple = image.size
+            # Квадрат размером 50x50
+            canvas.rectangle([size[0] / 2 - 25,size[1] / 2 - 25, \
+                size[0] / 2 + 25,size[1] / 2 + 25])
+            canvas.multiline_text((size[0] / 2 - 20, size[1] / 2 - 20), 'Hello,\nWorld!')
+            image.save(os.path.splitext(im)[0] + to_ext)
+            del canvas
+        except:
+            print(f'Ошибка при попытке изменения расширения изображения {im}')
